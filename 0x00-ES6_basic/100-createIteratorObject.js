ca8518858ecf8f1b7850478fb6f20a1d30998dc9
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-05 00:26:53 +0300

    fix minor issue

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,13 @@
-export default function createReportObject(employeesList) {
-  return {
-    allEmployees: employeesList,
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
-    },
-  };
+export default function createIteratorObject(report) {
+  const employees = Object.values(report);
+  const allEmployees = [];
+
+  for (const department of employees) {
+    allEmployees.push(...department);
+  }
+  return (function* () {
+    for (const employee of allEmployees) {
+      yield employee;
+    }
+  }());
 }

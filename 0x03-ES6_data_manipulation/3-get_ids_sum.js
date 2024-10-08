04a8b0c17120e73ef1748b8e6f6d2375d8ac3669
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-09 02:31:55 +0300

    fix typo

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,6 +2,7 @@
  * Retrieves the sum of ids of a list of students.
  */
 export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
     return students.reduce(
       (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
       0,

f49f194309ade3426eedb4661c1030060f31d74f
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-09 02:16:14 +0300

    fix minor issue

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,18 @@
 /**
  * Changes the quantity of unique grocery items to 100.
  */
-export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) {
-    throw new Error("Can't process");
+export default function updateUniqueItems(groceriesMap) {
+  // Check if the argument is a Map
+  if (!(groceriesMap instanceof Map)) {
+    throw new Error("Cannot process");
   }
-  map.forEach((value, key) => {
+
+  // Iterate over the Map entries
+  groceriesMap.forEach((value, key) => {
     if (value === 1) {
-      map.set(key, 100);
+      groceriesMap.set(key, 100);  // Update quantity to 100 if it's 1
     }
   });
+
+  return groceriesMap;  // Return the updated Map
 }

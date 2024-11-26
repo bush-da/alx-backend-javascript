87b1c375f2eb1acb1e0659eb4b1e38467be7f466
Daniel Tujuma <danielsevani12@gmail.com>
2024-11-27 02:27:07 +0300

    add new line

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,5 @@
-module.exports = function displayMessage(msg) {
-    process.stdout.write(msg + '\n')
+function displayMessage(message) {
+    process.stdout.write(message + '\n'); // Print the message followed by a newline
 }
+
+module.exports = displayMessage; // Export the function for use in other files

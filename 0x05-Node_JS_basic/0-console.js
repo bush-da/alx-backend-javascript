a7400c33c27f6cf7c65648a62cd0ea048c153913
Daniel Tujuma <danielsevani12@gmail.com>
2024-11-27 03:15:04 +0300

    fix minor issue

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,8 +2,8 @@
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
  */
-function displayMessage(message) {
-    process.stdout.write(message + '\n');
-}
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 
 module.exports = displayMessage;

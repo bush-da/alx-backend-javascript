997ac8a5685c1a44ee828d1dcf51afd20968d39c
Daniel Tujuma <danielsevani12@gmail.com>
2024-11-27 03:43:59 +0300

    accept input and print

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,13 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Create a readline interface for user input
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-// Display the initial prompt
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  // Display the user's name after they input it
-  console.log(`Your name is: ${name}`);
-
-  // Close the readline interface and display the closing message
-  rl.close();
-});
-
-// Event listener for when the readline interface closes
-rl.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
